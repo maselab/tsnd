c2ca0ce48843004081b1d59e1a77f94aa56c2e08
enokibori <enokibori@i.nagoya-u.ac.jp>
2022-12-27 19:04:36 +0900

    Update download_saved_data sample app

--- sample_app/download_saved_data.py
+++ sample_app/download_saved_data.py
@@ -3,11 +3,9 @@ from queue import Queue
 import numpy as np
 import time
 
+path_to_serial_port="/dev/tty.TSND151-AP09181537"
 
-with TSND151.open(path_to_serial_port="/dev/tty.TSND151-AP09181537"
-        , wait_sec_on_open_for_stability=0.2
-        , wait_sec_on_auto_close_for_stability=0.2
-        ) as tsnd151:
+with TSND151.open(path_to_serial_port) as tsnd151:
     # init
     tsnd151.stop_recording()
     tsnd151.set_time()
@@ -24,12 +22,13 @@ with TSND151.open(path_to_serial_port="/dev/tty.TSND151-AP09181537"
 
     for i in range(total_entry_num):
         q = Queue()
-        tsnd151.set_response_queue('acc_gyro_data', q)
+        tsnd151.set_response_queue('acc_gyro_data', q)  # it should be arranged for the used recording setting
 
-        start_time = tsnd151.get_saved_entry_start_date(i+1)
+        start_time = tsnd151.get_saved_entry_start_date(i+1, remove_hms=False)
+        start_timestamp = start_time.replace(hour=0, minute=0, second=0, microsecond=0).timestamp() 
         get_saved_entry(i+1)
 
-        res = np.array([conv(q.get()) for i in range(q.qsize())])
+        res = np.array([conv(q.get(), start_timestamp) for i in range(q.qsize())])
 
         np.savetxt(f'{start_time.strftime("%Y-%m-%d_%H-%M-%S")}.csv', res, delimiter=',')
 
