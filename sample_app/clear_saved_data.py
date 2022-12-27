13fb34c894cbd456b2189352846ec23ab66e8e6d
enokibori <enokibori@i.nagoya-u.ac.jp>
2022-12-27 19:22:19 +0900

    Add a sample app

---
+++ sample_app/clear_saved_data.py
@@ -0,0 +1,24 @@
+from tsnd import TSND151
+from queue import Queue
+import numpy as np
+import time
+import sys
+
+path_to_serial_port="/dev/tty.TSND151-AP09181537"
+
+with TSND151.open(path_to_serial_port) as tsnd151:
+    # init
+    tsnd151.stop_recording()
+    tsnd151.set_time()
+
+    total_entry_num = tsnd151.get_saved_entry_num() 
+    print(f'saved entry num: {total_entry_num}')
+    print('clear saved data? [n/Y]')
+
+    line = sys.stdin.readline()
+    cmd = line.rstrip().lower()
+    if cmd == 'y' or cmd == 'yes':
+        tsnd151.clear_saved_entry()
+        print('clear')
+    else:
+        print('do nothing')
